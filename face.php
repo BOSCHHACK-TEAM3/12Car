730ae19baec5735415bba15fe2b01aaa79495a30
wenwen195 <1034637037@qq.com>
2017-06-03 17:26:41 +0800

    key for iot

--- face.php
+++ face.php
@@ -2,13 +2,13 @@
 // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
 require_once 'HTTP/Request2.php';
 
-$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/face/v1.0/identify');
+$request = new Http_Request2('https://api.cognitive.azure.cn/face/v1.0/identify');
 $url = $request->getUrl();
 
 $headers = array(
     // Request headers
     'Content-Type' => 'application/json',
-    'Ocp-Apim-Subscription-Key' => '{subscription key}',
+    'Ocp-Apim-Subscription-Key' => '{fff72c4d03e6462199e376449a16cfc0}',
 );
 
 $request->setHeader($headers);
